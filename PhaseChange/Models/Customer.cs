4424d8e8d8b07c1129ea0e458c3c774e2107b4eb
RM-Terrell <terrellrmr@gmail.com>
2017-03-13 18:42:19 -0600

    Validation message over rides and message CSS

--- PhaseChange/Models/Customer.cs
+++ PhaseChange/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace PhaseChange.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Enter customer's name")] //How to over ride message
         [StringLength(255)] //Data annotations
         public string Name { get; set; }
 
@@ -19,7 +19,8 @@ namespace PhaseChange.Models
         public MembershipType MembershipType { get; set; } //Navigation property
 
         [Display(Name = "Membership Type")] //Over rides display from default
-        public byte MembershipTypeId { get; set; }//Treated as a forgeign key automagically
+        public byte MembershipTypeId { get; set; }//Treated as a forgeign key automagically. 
+                                                  // Byte is implicitly requried. Use byte? if you want nullable
 
         [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; } //"?" makes it nullable
